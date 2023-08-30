6353a11f5a5a7dff06f7abb7592804a86fc13ba5
PetarNeshkov <petio567@abv.bg>
2023-08-30 04:29:23 +0300

    First Solution

---
+++ Async and Await Exercise/C-Sharp-Async-And-Await-Exercise/01. FromSyncToAsync/Program.cs
@@ -0,0 +1,28 @@
+﻿SimpleExample();
+ComplexExample();
+
+void SimpleExample()
+{
+    var task = Task.Run(() => Console.WriteLine("First!"));
+
+    Console.WriteLine("Second");
+
+    task.Wait();
+}
+
+void ComplexExample()
+{
+    var task = Task
+        .Run(() => Task
+            .Delay(2000)
+            .ContinueWith(t => "In a task"));
+
+    Task.Delay(4000).Wait();
+    Console.WriteLine("Outside of a task!");
+
+    var completion = Task
+        .WhenAll(task)
+        .ContinueWith(async t => { Console.WriteLine((await t)[0]); });
+
+    completion.Wait();
+}
