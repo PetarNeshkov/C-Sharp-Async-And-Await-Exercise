82b5c1ea7f1c8f15e29496c255d000b311a1fe9e
PetarNeshkov <petio567@abv.bg>
2023-09-01 23:07:29 +0300

    Exercise with race condition

---
+++ Async and Await Exercise/C-Sharp-Async-And-Await-Exercise/03. MultithreadingAndAsynchronous/Program.cs
@@ -0,0 +1,31 @@
+﻿List<int> data = Enumerable.Range(0, 100).ToList();
+
+for (int i = 0; i < 4; i++)
+{
+    var thread = new Thread(Work);
+
+    thread.Start();
+
+    if (i % 2 == 0)
+    {
+        thread.Join();
+    }
+
+    Console.WriteLine(data.Count);
+}
+
+void Work()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(i);
+
+        Thread.Sleep(500);
+
+        // Creates race condition
+        if (data.Count > 90)
+        {
+            data.RemoveAt(data.Count - 1);
+        }
+    }
+}
