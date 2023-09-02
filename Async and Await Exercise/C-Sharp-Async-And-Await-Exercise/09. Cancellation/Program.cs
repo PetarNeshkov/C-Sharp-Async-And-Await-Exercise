5c27df9e0f5d88aa88dd3622554cdc4aa1f74d52
PetarNeshkov <petio567@abv.bg>
2023-09-03 01:06:14 +0300

    Implemented cancellation token exercise

---
+++ Async and Await Exercise/C-Sharp-Async-And-Await-Exercise/09. Cancellation/Program.cs
@@ -0,0 +1,37 @@
+﻿var cancellation = new CancellationTokenSource(2000);
+
+var printTask = Task.Run(async () =>
+{
+    while (true)
+    {
+        if (cancellation.IsCancellationRequested)
+        {
+            Console.WriteLine("Enough!");
+
+            cancellation.Token.ThrowIfCancellationRequested();
+
+            break;
+        }
+
+        Console.WriteLine(DateTime.Now);
+
+        await Task.Delay(1000);
+    }
+}, cancellation.Token);
+
+var readTask = Task.Run(() =>
+{
+    while (true)
+    {
+        var input = Console.ReadLine();
+
+        if (input == "end")
+        {
+            cancellation.Cancel();
+
+            break;
+        }
+    }
+});
+
+await Task.WhenAll(printTask, readTask);
