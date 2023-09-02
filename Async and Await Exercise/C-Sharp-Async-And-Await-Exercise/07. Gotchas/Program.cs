f2c4be930b974ae36cb34e532bde3b1d0632bbd0
PetarNeshkov <petio567@abv.bg>
2023-09-02 18:38:25 +0300

    Implemented unreachable exception in code

---
+++ Async and Await Exercise/C-Sharp-Async-And-Await-Exercise/07. Gotchas/Program.cs
@@ -0,0 +1,57 @@
+﻿//RunAsyncVoid();
+//AsyncVoidLambda();
+
+await NestedTasks();
+
+async Task NestedTasks()
+{
+    await Task.Run(async () =>
+    {
+        Console.WriteLine("Before delay");
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("After Delay");
+    });
+
+    Console.WriteLine("After Task");
+}
+
+void AsyncVoidLambda()
+{
+    try
+    {
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+
+        list.ForEach(async number =>
+        {
+            await Task.Run(() => Console.WriteLine(number));
+
+            throw new InvalidOperationException("In a lambda!");
+        });
+    }
+    catch
+    {
+        Console.WriteLine("Cannot be caught!");
+    }
+}
+
+void RunAsyncVoid()
+{
+    try
+    {
+        AsyncVoid();
+    }
+    catch
+    {
+        Console.WriteLine("Cannot be caught!");
+        throw;
+    }
+}
+
+async void AsyncVoid()
+{
+    await Task.Run(() => Console.WriteLine("Message"));
+
+    throw new InvalidOperationException("Error");
+}
