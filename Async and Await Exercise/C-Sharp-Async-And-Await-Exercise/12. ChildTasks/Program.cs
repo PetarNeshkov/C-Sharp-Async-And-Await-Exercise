9eb83173373d73a942b994c844f435c1b8690790
PetarNeshkov <petio567@abv.bg>
2023-09-03 14:27:52 +0300

    Exercising attaching and detaching parent

---
+++ Async and Await Exercise/C-Sharp-Async-And-Await-Exercise/12. ChildTasks/Program.cs
@@ -0,0 +1,54 @@
+﻿//Detached();
+//Attached();
+
+await DetachedAsync();
+
+return;
+
+async Task DetachedAsync()
+{
+    Console.WriteLine("First task async");
+
+    await Task.Run(async () =>
+    {
+        await Task.Delay(4000);
+        Console.WriteLine("Second task async");
+    });
+
+    Console.WriteLine("Done");
+}
+
+void Attached()
+{
+    Task
+        .Factory
+        .StartNew(() =>
+        {
+            Console.WriteLine("First task attached");
+
+            Task.Factory.StartNew(() =>
+            {
+                Task.Delay(10000).Wait();
+
+                Console.WriteLine("Second task attached");
+            }, TaskCreationOptions.AttachedToParent);
+        })
+        .Wait();
+}
+
+void Detached()
+{
+    Task
+        .Factory
+        .StartNew(() =>
+        {
+            Console.WriteLine("First task detached");
+
+            Task.Factory.StartNew(() =>
+            {
+                Task.Delay(10000).Wait();
+                Console.WriteLine("Second task detached");
+            });
+        })
+        .Wait();
+}
